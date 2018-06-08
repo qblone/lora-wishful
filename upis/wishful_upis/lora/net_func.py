a30a9b44ad1c21e74707b26d29329b8c43bf24d0
Qasim <qasim.lone@gmail.com>
2018-06-08 15:41:06 +0200

    no message

--- upis/wishful_upis/lora/net_func.py
+++ upis/wishful_upis/lora/net_func.py
@@ -1,11 +1,6 @@
-__author__ = "Piotr Gawlowicz, Anatolij Zubow"
-__copyright__ = "Copyright (c) 2015, Technische Universitat Berlin"
+from wishful_upis.meta_models import Attribute, Measurement, Event, Action
+
+__author__ = "Qasim Lone"
+__copyright__ = "Copyright (c) 2016, Grenoble INP, IMAG, LIG LAB"
 __version__ = "0.1.0"
-__email__ = "{gawlowicz, zubow}@tkn.tu-berlin.de"
-
-'''IEEE 802.15.4 protocol family
-
-The protocol-specific definition of WiSHFUL global control interface, UPI_G,
-for performing network-wide operations which go beyond just remote UPI_R/N calls
-
-'''
+__email__ = "qasim.lone@univ-grenoble-alpes.fr"
